6301417d1b665274902fdc261eebac39a7ba82f7
Yui0909 <72923980+Yui0909@users.noreply.github.com>
2020-12-17 21:13:40 +0900

    Update myled.c

--- myled.c
+++ myled.c
@@ -1,3 +1,8 @@
+// SPDX-License-Identifier: GPL-3.0
+/*
+	Copyright (C) 2020  Ayaka Yui and Ryuichi Ueda. All right reserved.
+*/
+
 #include <linux/module.h>
 #include <linux/fs.h>
 #include <linux/cdev.h>
